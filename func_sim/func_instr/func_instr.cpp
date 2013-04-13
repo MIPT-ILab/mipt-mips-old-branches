f607b6b2fba05af712324094197f5088784ee01d
alexander.kravtcov <alexander.kravtcov@8191d7f6-0415-6e15-5cfd-e6cccd3c4e6a>
2013-04-13 20:37:09 +0000

    FuncInstr class is changed. Now one supports all necessary instructions without execution method
    git-svn-id: https://uarch-sim.googlecode.com/svn/branches/2012/akravtcov_task_4@285 8191d7f6-0415-6e15-5cfd-e6cccd3c4e6a

--- func_sim/func_instr/func_instr.cpp
+++ func_sim/func_instr/func_instr.cpp
@@ -1,10 +1,9 @@
 /**
  * func_instr.cpp - Implementation of the instruction decoder 
- * @author Alexander Kravtcov
+ * author Alexander Kravtcov
  */
 
 // Generic C
-#include <cerrno>
 #include <cstdlib>
 
 // Generic C++
@@ -14,6 +13,39 @@
 
 // uArchSim modules
 #include <func_instr.h>
+
+const FuncInstr::InstrInfo FuncInstr::ISA[] =
+{
+    /*     name      opcode  funct    type  s  t  d im order*/
+    {   ADD, "  add",  0x0,   0x20, R_TYPE, 1, 1, 1, 0, DST},
+    {  ADDU, " addu",  0x0,   0x21, R_TYPE, 1, 1, 1, 0, DST},
+    {   SUB, "  sub",  0x0,   0x22, R_TYPE, 1, 1, 1, 0, DST},
+    {  SUBU, " subu",  0x0,   0x23, R_TYPE, 1, 1, 1, 0, DST},
+    {  ADDI, " addi",  0x8, UNUSED, I_TYPE, 1, 1, 0, 1, TSI},
+    { ADDIU, "addiu",  0x9, UNUSED, I_TYPE, 1, 1, 0, 1, TSI},
+    {   SLL, "  sll",  0x0,    0x0, R_TYPE, 0, 1, 1, 1, DTI},
+    {   SRL, "  srl",  0x0,    0x2, R_TYPE, 0, 1, 1, 1, DTI},
+    {   BEQ, "  beq",  0x4, UNUSED, I_TYPE, 1, 1, 0, 1, STI},
+    {   BNE, "  bne",  0x5, UNUSED, I_TYPE, 1, 1, 0, 1, STI},
+    {     J, "    j",  0x2, UNUSED, J_TYPE, 0, 0, 0, 1,   I},
+    {    JR, "   jr",  0x0,    0x8, R_TYPE, 1, 0, 0, 0,   S},
+    {    LW, "   lw", 0x23, UNUSED, I_TYPE, 1, 1, 0, 1, TIS},
+    {    SW, "   sw", 0x2B, UNUSED, I_TYPE, 1, 1, 0, 1, TIS},
+    {   LUI, "  lui",  0xF, UNUSED, I_TYPE, 0, 1, 0, 1,  TI}
+};
+
+const FuncInstr::RegName FuncInstr::RegNames[] =
+{ 
+    { ZERO, "$zero"}, { AT, "$at"}, { V0, "$v0"}, { V1, "$v1"},
+    {   A0,   "$a0"}, { A1, "$a1"}, { A2, "$a2"}, { A3, "$a3"},
+    {   T0,   "$t0"}, { T1, "$t1"}, { T2, "$t2"}, { T3, "$t3"}, 
+    {   T4,   "$t4"}, { T5, "$t5"}, { T6, "$t6"}, { T7, "$t7"},
+    {   S0,   "$s0"}, { S1, "$s1"}, { S2, "$s2"}, { S3, "$s3"}, 
+    {   S4,   "$s4"}, { S5, "$s5"}, { S6, "$s6"}, { S7, "$s7"}, 
+    {   T8,   "$t8"}, { T9, "$t9"}, { K0, "$k0"}, { K1, "$k1"}, 
+    {   GP,   "$gp"}, { SP, "$sp"}, { FP, "$fp"}, { RA, "$ra"}
+};
+
 
 FuncInstr::FuncInstr( uint32 bytes)
 {
@@ -25,10 +57,19 @@ FuncInstr::FuncInstr( uint32 bytes)
 
     this->num_of_arguments = this->instr.is_reg_s + this->instr.is_reg_t +
                              this->instr.is_reg_d + this->instr.is_imm;
+
+    formDumpStr();
 }
 
-InstrInfo FuncInstr::selection() const
+FuncInstr::InstrInfo FuncInstr::selection() const
 {
+
+#if DEBUG        
+    std::cout << "opcode: " << std::hex << this->convertible.asR.opcode 
+              << std::dec << ", funct: " << std::hex
+              << this->convertible.asR.funct << std::dec << std::endl;
+#endif
+
     for ( int i = 0; i < SIZE_OF_ISA; i++)
     {
         if ( ( this->convertible.asR.opcode == ISA[ i].opcode) &&
@@ -77,95 +118,104 @@ void FuncInstr::parseInstr()
     }
 
     // pseudo-instructions
-    if ( ( this->instr.name == "addiu") && ( this->imm == 0))
+    if ( ( this->instr.name == ADDIU) && ( this->imm == 0))
     {
-        this->instr.name = "move";
+        this->instr.name_str = " move";
         this->instr.is_imm = 0;
+        this->instr.arg_order = TS;
     }
-    if ( ( this->instr.name == "addu") && ( this->reg_t == "$zero"))
+    if ( ( this->instr.name == ADDU) && ( this->reg_t.reg == ZERO))
     {
-        this->instr.name = "move";
+        this->instr.name_str = " move";
         this->instr.is_reg_t = 0;
+        this->instr.arg_order = TS;
     }
-    if ( ( this->instr.name == "addu") && ( this->reg_s == "$zero") &&
-         ( this->reg_d == "$zero"))
+    if ( ( this->instr.name == ADDU) && ( this->reg_s.reg == ZERO) &&
+         ( this->reg_d.reg == ZERO))
     {
-        this->instr.name = "clear";
+        this->instr.name_str = "clear";
         this->instr.is_reg_s = 0;
         this->instr.is_reg_d = 0;
+        this->instr.arg_order = T;
     }
-    if ( ( this->instr.name == "sll") && ( this->reg_t == "$zero") &&
-         ( this->reg_d == "$zero") && ( this->imm == 0))
+    if ( ( this->instr.name == SLL) && ( this->reg_t.reg == ZERO) &&
+         ( this->reg_d.reg == ZERO) && ( this->imm == 0))
     {
-        this->instr.name = "nop";
+        this->instr.name_str = "  nop";
         this->instr.is_reg_t = 0;
         this->instr.is_reg_d = 0;
         this->instr.is_imm = 0;
+        this->instr.arg_order = NOT;
     }
+}
+
+void FuncInstr::formDumpStr()
+{
+    std::ostringstream oss;
+
+    oss << this->instr.name_str << " ";
+    
+    switch ( this->instr.arg_order)
+    {
+        case DST:
+            oss << this->reg_d.name_str << ", " << this->reg_s.name_str << ", "
+                << this->reg_t.name_str;
+            break;
+        
+        case TSI:
+            oss << this->reg_t.name_str << ", " << this->reg_s.name_str << ", "
+                << "0x" << std::hex << this->imm << std::dec;
+            break;
+
+        case DTI:
+            oss << this->reg_d.name_str << ", " << this->reg_t.name_str << ", "
+                << "0x" << std::hex << this->imm << std::dec;
+            break;
+
+        case STI:
+            oss << this->reg_s.name_str << ", " << this->reg_t.name_str << ", "
+                << "0x" << std::hex << this->imm << std::dec;
+            break;
+
+        case TS:
+            oss << this->reg_t.name_str << ", " << this->reg_s.name_str;
+            break;
+
+        case I:
+            oss << "0x" << std::hex << this->imm << std::dec;
+            break;
+
+        case S:
+            oss << this->reg_s.name_str;
+            break;
+
+        case T:
+            oss << this->reg_t.name_str;
+            break;
+
+        case TIS:
+            oss << this->reg_t.name_str << ", " << this->imm 
+                << "(" << this->reg_s.name_str << ")";
+            break;
+
+        case TI:
+            oss << this->reg_t.name_str << ", "
+                << "0x" << std::hex << this->imm << std::dec;
+            break;
+
+        case NOT:
+            break;
+
+    }
+
+    this->dump_str = oss.str();
 }
 
 std::string FuncInstr::dump( std::string indent) const
 {
     std::ostringstream oss;
 
-    oss << indent << this->instr.name << " ";
-    
-    int printed_num_of_arguments = 0;
-
-    if ( this->instr.is_reg_d)
-    {
-        oss << this->reg_d;
-        if ( ++printed_num_of_arguments < this->num_of_arguments)
-        {
-            oss << ", ";
-        }
-    }
-    
-    // for this instructions ( "addi" and "addiu") register t must be before s
-    if ( !this->instr.is_right_order_reg)
-    {
-        if ( this->instr.is_reg_t)
-        {
-            oss << this->reg_t;
-            if ( ++printed_num_of_arguments < this->num_of_arguments)
-            {
-                oss << ", ";
-            }
-        }
-        if ( this->instr.is_reg_s)
-        {
-            oss << this->reg_s;
-            if ( ++printed_num_of_arguments < this->num_of_arguments)
-            {
-                oss << ", ";
-            }
-        }
-    } else
-    {
-        if ( this->instr.is_reg_s)
-        {
-            oss << this->reg_s;
-            if ( ++printed_num_of_arguments < this->num_of_arguments)
-            {
-                oss << ", ";
-            }
-        }
-        if ( this->instr.is_reg_t)
-        {
-            oss << this->reg_t;
-            if ( ++printed_num_of_arguments < this->num_of_arguments)
-            {
-                oss << ", ";
-            }
-        }
-    }
-
-    if ( this->instr.is_imm)
-    {
-        oss << "0x" << std::hex << this->imm << std::dec;
-    }
-
-    oss << std::endl;
+    oss << indent << this->dump_str << std::endl;
 
     return oss.str();
 }
--- func_sim/func_instr/func_instr.h
+++ func_sim/func_instr/func_instr.h
@@ -1,6 +1,6 @@
 /**
  * func_instr.h - Header of the instruction decoder 
- * @author Alexander Kravtcov
+ * author Alexander Kravtcov
  */
 
 // protection from multi-include
@@ -13,96 +13,133 @@
 // uArchSim modules
 #include <types.h>
 
-enum TypeFormat
-{
-    R_TYPE,
-    I_TYPE,
-    J_TYPE
-};
-
-union Convert
-{
-    uint32 bytes; // original convertible data
-    
-    struct // see on instruction as R-type 
-    {
-        unsigned funct:6;
-        unsigned shamt:5;
-        unsigned d:5;
-        unsigned t:5;
-        unsigned s:5;
-        unsigned opcode:6;
-    } asR;
-
-    struct
-    {
-        unsigned imm:16;
-        unsigned t:5;
-        unsigned s:5;
-        unsigned opcode:6;
-    } asI;
-
-    struct
-    {
-        unsigned imm:26;
-        unsigned opcode:6;
-    } asJ;
-};
-
-struct InstrInfo
-{
-    std::string name; // name of instruction ( e.g. "add", "sll")
-    unsigned opcode;
-    unsigned funct;
-    TypeFormat type;
-
-    // the presense or absense arguments
-    bool is_reg_s;
-    bool is_reg_t;
-    bool is_reg_d;
-    bool is_imm;
-
-    // in most MIPS-instructions order of registers is $d, $s ,$t
-    bool is_right_order_reg;
-};
-
-const unsigned UNUSED = -1; // for marking unused filds in list of instructions
-const int SIZE_OF_ISA = 12;
-
-const InstrInfo ISA[] = { { "add", 0x0, 0x20, R_TYPE, 1, 1, 1, 0, 1},
-                          { "addu", 0x0, 0x21, R_TYPE, 1, 1, 1, 0, 1},
-                          { "sub", 0x0, 0x22, R_TYPE, 1, 1, 1, 0, 1},
-                          { "subu", 0x0, 0x23, R_TYPE, 1, 1, 1, 0, 1},
-                          { "addi", 0x8, UNUSED, I_TYPE, 1, 1, 0, 1, 0},
-                          { "addiu", 0x9, UNUSED, I_TYPE, 1, 1, 0, 1, 0},
-                          { "sll", 0x0, 0x0, R_TYPE, 0, 1, 1, 1, 1},
-                          { "srl", 0x0, 0x2, R_TYPE, 0, 1, 1, 1, 1},
-                          { "beq", 0x4, UNUSED, I_TYPE, 1, 1, 0, 1, 1},
-                          { "bnq", 0x5, UNUSED, I_TYPE, 1, 1, 0, 1, 1},
-                          { "j", 0x2, UNUSED, J_TYPE, 0, 0, 0, 1, 1},
-                          { "jr", 0x0, 0x8, R_TYPE, 1, 0, 0, 0, 1}};
-
-const std::string RegNames[] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1",
-                                 "$a2", "$a3", "$t0", "$t1", "$t2", "$t3",
-                                 "$t4", "$t5", "$t6", "$t7", "$s0", "$s1",
-                                 "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
-                                 "$t8", "$t9", "$k0", "$k1", "$gp", "$sp",
-                                 "$fp", "$ra"};
-
 class FuncInstr
 {
+    // declaration some private types and constants
+    enum TypeFormat
+    {
+        R_TYPE,
+        I_TYPE,
+        J_TYPE
+    };
+    
+    union Convert
+    {
+        uint32 bytes; // original convertible data
+        
+        struct // see on instruction as R-type 
+        {
+            unsigned funct:6;
+            unsigned shamt:5;
+            unsigned d:5;
+            unsigned t:5;
+            unsigned s:5;
+            unsigned opcode:6;
+        } asR;
+    
+        struct
+        {
+            unsigned imm:16;
+            unsigned t:5;
+            unsigned s:5;
+            unsigned opcode:6;
+        } asI;
+    
+        struct
+        {
+            unsigned imm:26;
+            unsigned opcode:6;
+        } asJ;
+    };
+    
+    enum ArgOrderPrint
+    {
+        DST, // will be printed: $d, $s, $t
+        TSI, // will be printed: $s, $t, IMM
+        DTI,
+        STI,
+        TS,
+        I,
+        S,
+        T,
+        TIS, // will be printed: $t, IMM($t)
+        TI,
+        NOT // no argement will be printed
+    };
+    
+    enum InstrNames
+    {
+       ADD, ADDU, SUB, SUBU, ADDI, ADDIU, SLL, SRL, BEQ, BNE, J, JR, LW, SW, LUI
+    };
+    
+    struct InstrInfo
+    {
+        InstrNames name;
+        std::string name_str; // printed name of instruction (e.g. "add", "sll")
+        unsigned opcode;
+        unsigned funct;
+        TypeFormat type;
+    
+        // the presense or absense arguments
+        bool is_reg_s;
+        bool is_reg_t;
+        bool is_reg_d;
+        bool is_imm;
+    
+        ArgOrderPrint arg_order;
+    };
+    
+    // for marking unused filds in list of instructions
+    static const unsigned UNUSED = -1;
+
+    static const int SIZE_OF_ISA = 15;
+    static const int SIZE_OF_REGISTERS = 32;
+    
+    static const InstrInfo ISA[ SIZE_OF_ISA]; // define in .cpp file
+    
+    enum Registers
+    {
+        ZERO, AT,
+        V0, V1,
+        A0, A1, A2, A3,
+        T0, T1, T2, T3, T4, T5, T6, T7,
+        S0, S1, S2, S3, S4, S5, S6, S7,
+        T8, T9,
+        K0, K1,
+        GP, SP, FP, RA
+    };
+    
+    struct RegName
+    {
+        enum Registers reg;
+        std::string name_str;
+    };
+    
+    static const RegName RegNames[ SIZE_OF_REGISTERS]; // define in .cpp file
+    // end of declaration some private types and constants
+
+
+
     InstrInfo instr;
 
     unsigned imm;  // contain constant, which can be in instruction
-    std::string reg_s; //
-    std::string reg_t; // registers
-    std::string reg_d; //
+    RegName reg_s; //
+    RegName reg_t; // registers
+    RegName reg_d; //
 
     int num_of_arguments; // which are after name of instruction
 
+    // contain command with argements for print ("add $s0, %t0, %t1")
+    std::string dump_str; 
+
     Convert convertible;
 
-    InstrInfo selection() const; // from list of ISA
+    void formDumpStr();
+
+    // define what is the instruction via choice from the list of ISA
+    InstrInfo selection() const;
+
+    // fills registers and imm fields and checks pseudo-instructions
     void parseInstr();
 
     // for unusing default constructor
@@ -116,3 +153,4 @@ public:
 std::ostream& operator<<( std::ostream& out, const FuncInstr& instr);
 
 #endif // #ifndef FUNC_INSTR__FUNC_INSTR_H
+
