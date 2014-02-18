0196738fcc48f264fed4bf12877da45b17ae7363
vladislav.fofanov@frtk.ru <vladislav.fofanov@frtk.ru@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-02-18 15:33:35 +0000

    [branch] Added some coments and fix coding style
    git-svn-id: https://mipt-mips.googlecode.com/svn/branches/2013/vfofanov_task_2@253 7b285467-9990-9c3a-2c35-738ad0b76374

--- func_sim/func_instr/func_instr.cpp
+++ func_sim/func_instr/func_instr.cpp
@@ -2,167 +2,185 @@
 
 //------------------------------------------------------------------
 
-// Конструктор класса FuncInstr
+// Constructor ofclass FuncInstr
 
-FuncInstr::FuncInstr(uint32 bytes)
+FuncInstr::FuncInstr( uint32 bytes)
 {
 	bytes_.raw = bytes;
 
-// Случай, когда инструкция имеет R-формат
-	if(bytes_.asR.opcode == 0x00)
+// When instruction have R-format
+	if( bytes_.asR.opcode == 0x00)
 	{
-		InstrInfo.opcode = (uint8)bytes_.asR.opcode;
-		InstrInfo.funct = (uint8)bytes_.asR.funct;
-		InstrInfo.shamt = (uint32)bytes_.asR.shamt;
+// Enter same field for R-format instructions
+		InstrInfo.opcode = ( uint8)bytes_.asR.opcode;
+		InstrInfo.funct = ( uint8)bytes_.asR.funct;
+		InstrInfo.shamt = ( uint32)bytes_.asR.shamt;
 		
-		InstrInfo.rs = registers[bytes_.asR.s];
-		InstrInfo.rt = registers[bytes_.asR.t];
-		InstrInfo.rd = registers[bytes_.asR.d];
+		InstrInfo.rs = registers[ bytes_.asR.s];
+		InstrInfo.rt = registers[ bytes_.asR.t];
+		InstrInfo.rd = registers[ bytes_.asR.d];
 
 		InstrInfo.imm = NO_IMM;
 
 		InstrInfo.format = "FORMAT_R";
-		
-		if(InstrInfo.funct == 0x00)
+// SLL
+		if( InstrInfo.funct == 0x00)
 		{
 			InstrInfo.type = "SLL";
-			InstrInfo.rs = registers[32];
-		} 
-		else if(InstrInfo.funct == 0x02)
+			InstrInfo.rs = registers[ 32];
+		}
+// SRL 
+		else if( InstrInfo.funct == 0x02)
 		{
 			InstrInfo.type = "SRL";
-			InstrInfo.rs = registers[32]; 
+			InstrInfo.rs = registers[ 32]; 
 		}
-		else if(InstrInfo.funct == 0x08)
+// JR
+		else if( InstrInfo.funct == 0x08)
 		{
 			InstrInfo.type = "JR";
-			InstrInfo.rt = registers[32];
-			InstrInfo.rd = registers[32];
+			InstrInfo.rt = registers[ 32];
+			InstrInfo.rd = registers[ 32];
 			InstrInfo.shamt = NO_SHAMT;
 		}
-		else if(InstrInfo.funct == 0x20)
+// ADD
+		else if( InstrInfo.funct == 0x20)
 		{
 			InstrInfo.type = "ADD";
 			InstrInfo.shamt = NO_SHAMT;
 		}
-		else if(InstrInfo.funct == 0x21)
+// ADDU
+		else if( InstrInfo.funct == 0x21)
 		{
 			InstrInfo.type = "ADDU";
 			InstrInfo.shamt = NO_SHAMT;
 		}
-		else if(InstrInfo.funct == 0x22)
+// SUB
+		else if( InstrInfo.funct == 0x22)
 		{
 			InstrInfo.type = "SUB";
 			InstrInfo.shamt = NO_SHAMT;
 		}
-		else if(InstrInfo.funct == 0x23)
+// SUBU
+		else if( InstrInfo.funct == 0x23)
 		{
 			InstrInfo.type = "SUBU";
 			InstrInfo.shamt = NO_SHAMT;
 		}
-		else if(InstrInfo.funct == 0x0C)
+// SYSCALL
+		else if( InstrInfo.funct == 0x0C)
 		{
 			InstrInfo.type = "SYSCALL";
 			InstrInfo.shamt = NO_SHAMT;
-			InstrInfo.rs = registers[32];
-			InstrInfo.rt = registers[32];
+			InstrInfo.rs = registers[ 32];
+			InstrInfo.rt = registers[ 32];
 			InstrInfo.rd = registers[32];
 		}
-		// Иначе, инструкция имеет неизвестный формат
+// Else instruction have unknown type
 		else
 		{
-			cout << "instruction not defined   ";
-			InstrInfo.format = "NO_FORMAT";
-                	InstrInfo.type = "NO_TYPE";
-                	InstrInfo.opcode = INSTR_NOT_DEF;
-                	InstrInfo.rs = registers[32];
-                	InstrInfo.rt = registers[32];
-                	InstrInfo.rd = registers[32];
-                	InstrInfo.shamt = NO_SHAMT;
-                	InstrInfo.imm = NO_IMM;
+            InstrInfo.type = "NO_TYPE";
+            InstrInfo.opcode = INSTR_NOT_DEF;
+            InstrInfo.rs = registers[ 32];
+            InstrInfo.rt = registers[ 32];
+            InstrInfo.rd = registers[ 32];
+            InstrInfo.shamt = NO_SHAMT;
+            InstrInfo.imm = NO_IMM;
 		}
 	} 
  
-// Случай, когда инструкция имеет I-формат
-	else if((bytes_.asI.opcode == 0x04) || (bytes_.asI.opcode == 0x05) || (bytes_.asI.opcode == 0x08) || (bytes_.asI.opcode == 0x09) || (bytes_.asI.opcode == 0x23) || (bytes_.asI.opcode == 0x2B) || (bytes_.asI.opcode == 0x0F))
+// When instruction have I-format
+	else if( ( bytes_.asI.opcode == 0x04) || ( bytes_.asI.opcode == 0x05) ||  \
+	( bytes_.asI.opcode == 0x08) || ( bytes_.asI.opcode == 0x09) || 	      \
+	( bytes_.asI.opcode == 0x23) || ( bytes_.asI.opcode == 0x2B) ||           \
+	( bytes_.asI.opcode == 0x0F))
 	{
-		InstrInfo.opcode = (uint8)bytes_.asI.opcode;
+// Enter same field for I-type instructions
+		InstrInfo.opcode = ( uint8)bytes_.asI.opcode;
 		InstrInfo.funct = NO_FUNCT;
 		InstrInfo.shamt = NO_SHAMT;
 
-		InstrInfo.rs = registers[bytes_.asI.s];
+		InstrInfo.rs = registers[ bytes_.asI.s];
 		InstrInfo.rt = registers[bytes_.asI.t];
-		InstrInfo.rd = registers[32];
+		InstrInfo.rd = registers[ 32];
 		
-		InstrInfo.imm = (uint32)bytes_.asI.imm;
+		InstrInfo.imm = ( uint32)bytes_.asI.imm;
 		
 		InstrInfo.format = "FORMAT_I";
-
-		if(InstrInfo.opcode == 0x04)
+// BEQ
+		if( InstrInfo.opcode == 0x04)
 		{
 			InstrInfo.type = "BEQ";
 		}
-		else if(InstrInfo.opcode == 0x05)
+// BNE
+		else if( InstrInfo.opcode == 0x05)
 		{
 			InstrInfo.type = "BNE";
 		}
-		else if(InstrInfo.opcode == 0x08)
+// ADDI
+		else if( InstrInfo.opcode == 0x08)
 		{
 			InstrInfo.type = "ADDI";
 		}
-		else if(InstrInfo.opcode == 0x09)
+// ADDIU
+		else if( InstrInfo.opcode == 0x09)
 		{
 			InstrInfo.type = "ADDIU";
 		}
-		else if(InstrInfo.opcode == 0x23)
+// LW
+		else if( InstrInfo.opcode == 0x23)
 		{
 			InstrInfo.type = "LW";
 		}
-		else if(InstrInfo.opcode == 0x2B)
+// SW
+		else if( InstrInfo.opcode == 0x2B)
 		{
 			InstrInfo.type = "SW";
 		}
-		else if(InstrInfo.opcode == 0x0F)
+// LUI
+		else if( InstrInfo.opcode == 0x0F)
 		{
 			InstrInfo.type = "LUI";
-			InstrInfo.rs = registers[32];
+			InstrInfo.rs = registers[ 32];
 		}
 	}
 
-// Случай, когда инструкция имеет J-формат
-	else if((bytes_.asJ.opcode == 0x02) || (bytes_.asJ.opcode == 0x03))
+// When instruction have J-format
+	else if( ( bytes_.asJ.opcode == 0x02) || ( bytes_.asJ.opcode == 0x03))
 	{
-		InstrInfo.opcode = (uint8)bytes_.asJ.opcode;
+// Enter same field for J-type instructions
+		InstrInfo.opcode = ( uint8)bytes_.asJ.opcode;
 		InstrInfo.funct = NO_FUNCT;
 		InstrInfo.shamt = NO_SHAMT;
 		
-		InstrInfo.rs = registers[32];
-		InstrInfo.rt = registers[32];
-		InstrInfo.rd = registers[32];
+		InstrInfo.rs = registers[ 32];
+		InstrInfo.rt = registers[ 32];
+		InstrInfo.rd = registers[ 32];
 		
-		InstrInfo.imm = (uint32)bytes_.asJ.imm;
+		InstrInfo.imm = ( uint32)bytes_.asJ.imm;
 		
 		InstrInfo.format = "FORMAT_J";
-		if(InstrInfo.opcode == 0x02)
+// J
+		if( InstrInfo.opcode == 0x02)
 		{
 			InstrInfo.type = "J";
 		}
-		else if(InstrInfo.opcode == 0x03)
+// JAL
+		else if( InstrInfo.opcode == 0x03)
 		{
 			InstrInfo.type = "JAL";
 		}
 	}
 
-// Иначе, инструкция имеет неизвестный формат
+// Else instruction have unknown format
 	else
 	{
-		cout << "instruction not defined   ";
 		InstrInfo.format = "NO_FORMAT";
 		InstrInfo.type = "NO_TYPE";
 		InstrInfo.opcode = INSTR_NOT_DEF;
-		InstrInfo.rs = registers[32];
-		InstrInfo.rt = registers[32];
-		InstrInfo.rd = registers[32];
+		InstrInfo.rs = registers[ 32];
+		InstrInfo.rt = registers[ 32];
+		InstrInfo.rd = registers[ 32];
 		InstrInfo.shamt = NO_SHAMT;
 		InstrInfo.imm = NO_IMM;		
 	}
@@ -170,36 +188,50 @@ FuncInstr::FuncInstr(uint32 bytes)
 
 // -----------------------------------------------------------------
 
-// Функция, выводящая инструкцию на экран
+// Method, which print instruction
 
-string FuncInstr::Dump(string indent)
+string FuncInstr::Dump( string indent)
 {
-	if(InstrInfo.type != "NO_TYPE")
+// If instruction have unknown type
+    if( InstrInfo.type == "NO_TYPE")
+    {
+        cout << "---instruction is not defined---\n";
+        return " ";
+    }
+// Print type of instruction
+	if( InstrInfo.type != "NO_TYPE")
 	{
 		cout << indent << InstrInfo.type << " ";
 	}
-	if(InstrInfo.rs != registers[32])
+// Printf (if defined) rs
+	if( InstrInfo.rs != registers[ 32])
 	{
 		cout << InstrInfo.rs << " ";
 	} 
-	if(InstrInfo.rt != registers[32])
+// Printf (if defined) rt
+	if( InstrInfo.rt != registers[ 32])
 	{
 		cout << InstrInfo.rt << " ";
 	}
-	if(InstrInfo.rd != registers[32])
+// Printf (if defined) rd
+	if( InstrInfo.rd != registers[ 32])
 	{
 		cout << InstrInfo.rd << " ";
 	}
-	if(InstrInfo.shamt != NO_SHAMT)
+// Printf (if defined) shamt
+	if( InstrInfo.shamt != NO_SHAMT)
 	{
 		cout << InstrInfo.shamt << " ";
 	}
-	if(InstrInfo.imm != NO_IMM)
+// Printf (if defined) immidiate
+	if( InstrInfo.imm != NO_IMM)
 	{
-		if(InstrInfo.format == "FORMAT_I")
+// If size of immidiate == 2 bytes
+		if( InstrInfo.format == "FORMAT_I")
 		{
-			cout << (int16)InstrInfo.imm;
+			cout << ( int16)InstrInfo.imm;
 		}
+// If size of immidiate > 2 bytes
 		else
 		{
 			cout << InstrInfo.imm;
--- func_sim/func_instr/func_instr.h
+++ func_sim/func_instr/func_instr.h
@@ -8,12 +8,17 @@
 
 using namespace std;
 
+// If field "shamt" don't need, shamt = NO_SHAMT
 static const uint32 NO_SHAMT = 0xFFFFFFFF;
+// If field "funct" don't need, funct = NO_FUNCT
 static const uint8 NO_FUNCT = 0xFF;
+// If field "immidiate" don't need, imm = NO_IMM
 static const uint32 NO_IMM = 0xFFFFFFFF;
+// If instruction don't defined, opcode = INSTR_NOT_DEF
 static const uint8 INSTR_NOT_DEF = 0xFF;
 
-static string registers[33] = 					
+// This array have all registers name, each register meet his number in registers file
+static string registers[ 33] = 					
 {								
 	"$zero",						
 	"$at",							
@@ -30,10 +35,11 @@ static string registers[33] =
 	"NOTHING"						
 };
 
+// Main struct, which have all information about instruction
 struct InstrData
 {
-        uint8 opcode;
-        uint8 funct;
+    uint8 opcode;
+    uint8 funct;
 	uint32 shamt;
 
 	string rs;
@@ -46,8 +52,10 @@ struct InstrData
 	string type;
 };
 
+// This union needs for reading binary instruction in comfortable format
 union bytes_t
 {
+// For reading in I-type
 	struct
 	{
 		unsigned imm:16;
@@ -55,6 +63,7 @@ union bytes_t
 		unsigned s:5;
 		unsigned opcode:6;
 	} asI;
+// For reading in R-type
 	struct
 	{
 		unsigned funct:6;
@@ -64,23 +73,28 @@ union bytes_t
 		unsigned s:5;
 		unsigned opcode:6;
 	} asR;
+// For reading im J-type
 	struct
 	{
 		unsigned imm:26;
 		unsigned opcode:6;
 	} asJ;
+// For reading instruction in binary format
 	uint32 raw;
 };
 
+// Main class, which have info about instruction and some methods, which use this instruction
 class FuncInstr
 {
-	private:	
+	private:
+// Binary instruction	
 		bytes_t bytes_;
+// Info about instruction
 		InstrData InstrInfo;
 
 	public:
-		FuncInstr(uint32 bytes);
-		string Dump(string indent = " ");
+// Constructor of this class. He is parse binary instruction and enter info about it in InstrInfo
+		FuncInstr( uint32 bytes);
+// Dump method. He is print instruction in assembler format
+		string Dump( string indent = " ");
 };
-
-//static::InstrData InstrArray[10];
