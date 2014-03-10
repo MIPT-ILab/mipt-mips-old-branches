2f23bdd5555790cfafc567cdb91e606618c7751c
gusarov.aa@gmail.com <gusarov.aa@gmail.com@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-03-10 07:07:07 +0000

    [branches] Fixed minor bug.
    git-svn-id: https://mipt-mips.googlecode.com/svn/branches/2013/agusarov_task_2@280 7b285467-9990-9c3a-2c35-738ad0b76374

--- func_sim/func_instr/func_instr.cpp
+++ func_sim/func_instr/func_instr.cpp
@@ -43,7 +43,7 @@ const std::string FuncInstr::regTable[REGISTERS_COUNT] =
         "$zero",
         "$at",
         "$v0", "$v1",
-        "$a0", "$a2", "$a3",
+        "$a0", "$a1", "$a2", "$a3",
         "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
         "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
         "$t8", "$t9",
