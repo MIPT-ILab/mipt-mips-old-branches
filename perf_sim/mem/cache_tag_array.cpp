c2dff2bff11344724484bec2029e40569198ffd6
anton.v.mitrokhin@gmail.com <anton.v.mitrokhin@gmail.com@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-04-05 19:25:53 +0000

    [branch] improved protection from invalid arguments
    git-svn-id: https://mipt-mips.googlecode.com/svn/branches/2013/amitrokhin_task_3@304 7b285467-9990-9c3a-2c35-738ad0b76374

--- perf_sim/mem/cache_tag_array.cpp
+++ perf_sim/mem/cache_tag_array.cpp
@@ -7,6 +7,11 @@
 #include "cache_tag_array.h"
 
 
+bool powerOfTwo(unsigned int x)
+{
+    return !( x == 0) && !( x & ( x - 1));
+};
+
 
 
 CacheTagArray::CacheTagArray( unsigned int size_in_bytes,
@@ -17,32 +22,38 @@ CacheTagArray::CacheTagArray( unsigned int size_in_bytes,
     unsigned int data_blocks = size_in_bytes/block_size_in_bytes;
     unsigned int set_num     = data_blocks/ways;
 
-    this->offset_size = ceil(log(double(block_size_in_bytes))/log(2.0)); // base 2 logarithm
-    this->index_size  = ceil(log(double(set_num))/log(2.0));
-    if(addr_size_in_bits < index_size + offset_size)
+    if( !powerOfTwo( block_size_in_bytes) || !powerOfTwo(set_num))
     {
-        std::cerr << "Invalid address size (too short). terminating...";
+        std::cerr << "Invalid input sizes. terminating..." << std::endl;
+        exit(-1);
+    }
+
+    this->offset_size = ceil( log(double( block_size_in_bytes))/log( 2.0)); // base 2 logarithm
+    this->index_size  = ceil( log(double( set_num))/log( 2.0));
+    if( addr_size_in_bits < index_size + offset_size)
+    {
+        std::cerr << "Invalid address size (too short). terminating..." << std::endl;
         exit(-1);
     }
     this->tag_size = addr_size_in_bits - index_size - offset_size;
 
     for(unsigned int i = 0; i < set_num; i++)
     {
-        Set set(ways, this->offset_size);
-        this->sets.push_back(set);
+        Set set( ways, this->offset_size);
+        this->sets.push_back( set);
     }
 };
 
 
 bool CacheTagArray::read( uint64 addr)
 {
-    return this->sets[this->get_index(addr)].read(addr);
+    return this->sets[ this->get_index( addr)].read( addr);
 };
 
 
 void CacheTagArray::write( uint64 addr)
 {
-    this->sets[this->get_index(addr)].write(addr);
+    this->sets[ this->get_index(addr)].write( addr);
 };
 
 
@@ -51,10 +62,10 @@ uint64 CacheTagArray::get_index( uint64 addr)
     uint64 index = addr;
     uint64 mask = 0;
     mask = ~mask;
-    index = (index >> this->offset_size);
-    mask = ~(mask << this->index_size);
+    index = ( index >> this->offset_size);
+    mask = ~( mask << this->index_size);
 
-    index = (index & mask);
+    index = ( index & mask);
     return index;
 };
 
@@ -67,7 +78,7 @@ Set::Set( unsigned int _ways, uint64 _offset_size)
     for( unsigned int i = 0; i < this->ways; i++)
     {
         CacheLine cl;
-        this->lines.push_back(cl);
+        this->lines.push_back( cl);
     }
 };
 
@@ -75,7 +86,7 @@ Set::Set( unsigned int _ways, uint64 _offset_size)
 void Set::spin_once()
 {
     for( unsigned int i = 0; i < this->ways; i++)
-    this->lines[i].spin_once();
+    this->lines[ i].spin_once();
 };
 
 
@@ -85,17 +96,17 @@ void Set::write( uint64 addr)
     uint64 id = 0;
     for( unsigned int i = 0; i < this->ways; i++)
     {
-        if( this->lines[i].get_misusage_count() > max_misusage_count)
+        if( this->lines[ i].get_misusage_count() > max_misusage_count)
         {
             id = i;
-            max_misusage_count = this->lines[i].get_misusage_count();
+            max_misusage_count = this->lines[ i].get_misusage_count();
         }
     }
     // Actually, we'll get tag+index here, but since all indexes are
     // Equal in the set it does not matter
-    this->lines[id].set_valid(); // Set the valid bit
-    this->lines[id].write(this->cut_offset(addr)); // Actually remember that we remember the data :)
-    this->lines[id].touch(); // Set misusage counter to zero
+    this->lines[ id].set_valid(); // Set the valid bit
+    this->lines[ id].write( this->cut_offset(addr)); // Actually remember that we remember the data :)
+    this->lines[ id].touch(); // Set misusage counter to zero
 };
 
 bool Set::read( uint64 addr)
@@ -103,7 +114,7 @@ bool Set::read( uint64 addr)
     this->spin_once(); // Increment all misusage counters
     uint64 tag = this->cut_offset(addr);
     for( unsigned int i = 0; i < this->ways; i++)
-        if(this->lines[i].are_tags_equal(tag)) return true;
+        if(this->lines[ i].are_tags_equal( tag)) return true;
     return false;
 };
 
@@ -111,8 +122,8 @@ uint64 Set::cut_offset(uint64 addr)
 {
     uint64 mask = 0;
     mask = ~mask; // mask = 1111...11
-    mask = (mask << this->offset_size); // mask = 11...11110...00
-    return (addr & mask); // addr = xxxxxxxx0...00
+    mask = ( mask << this->offset_size); // mask = 11...11110...00
+    return ( addr & mask); // addr = xxxxxxxx0...00
 };
 
 
