9553a10b1d28a640f26e36c173ef6a63058249b1
aleksandr.kajda@frtk.ru <aleksandr.kajda@frtk.ru@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-04-12 22:08:49 +0000

    [branch] Final version of 3d assignment.
    git-svn-id: https://mipt-mips.googlecode.com/svn/branches/2013/akayda_task_3@328 7b285467-9990-9c3a-2c35-738ad0b76374

--- perf_sim/mem/cache_tag_array.cpp
+++ perf_sim/mem/cache_tag_array.cpp
@@ -1,123 +1,141 @@
 /*
- * cache_tag_array.cpp
- *
+ * cache_tag_array.cpp - perfomance simulator for 1-level cache
+ * @author Aleksandr Kayda aleksandr.kajda@frtk.ru
+ * Copyright 2014 uArchSim iLab 
  */
 
-
-
 #include <cache_tag_array.h>
-#include <iostream>
 #include <assert.h>
 
 #define NOT_LOG2 0xff
 
-// Very bad taking binary logarithm
-uint8 log2( uint32 value)
+/* Compute binary log, 
+ * returns NOT_LOG2 if is not integer power.
+ * Static non-member function. Hope this is not bad style.
+ */
+static uint8 log2( uint32 value)
 {
-	uint8 i = 0;
-	while ( value != 1)
-	{
-		if ( (value & 1) != 0)
-			return NOT_LOG2;
-		value >>= 1;
-		i++;
-	}
-	return i;
+    uint8 i = 0;
+    assert( value != 0);
+    while ( value != 1)
+    {
+        if ( ( value & 1) != 0)
+            return NOT_LOG2;
+        value >>= 1;
+        i++;
+    }
+    return i;
 }
 
 
 CacheTagArray::CacheTagArray( uint32 size_in_bytes,
-               				  uint32 ways,
-               				  uint16 block_size_in_bytes, 
-  				              uint16 addr_size_in_bits)
+                                 uint32 ways,
+                                 uint16 block_size_in_bytes, 
+                                uint16 addr_size_in_bits)
 {
-	cache_size = size_in_bytes;
-	ways_num = ways;
-	block_size = block_size_in_bytes;
-	addr_size = addr_size_in_bits;
+    cache_size = size_in_bytes;
+    ways_num = ways;
+    block_size = block_size_in_bytes;
+    addr_size = addr_size_in_bits;
 
-	assert( cache_size != 0);
-	assert( block_size != 0);
-	assert( addr_size != 0);
-	assert( addr_size <= 64);
-	if ( ways_num == 0) 	// it means full associativity
-		ways_num = cache_size / block_size;
+    assert( cache_size != 0);
+    assert( block_size != 0);
+    assert( addr_size != 0);
+    assert( addr_size <= 64);
+    if ( ways_num == 0)                 // it means full associativity
+        ways_num = cache_size / block_size;
 
-	offset_bits = log2(block_size);
-	assert( offset_bits != NOT_LOG2);
-	index_num = cache_size / ways_num / block_size;
-	assert( index_num != 0);
-	index_bits = log2(index_num); 		// Only power of two is permitted
-	assert( index_bits != NOT_LOG2);
-	tag_bits = addr_size - offset_bits - index_bits;
-	assert( tag_bits >= 0);
+    offset_bits = log2( block_size);
+    assert( offset_bits != NOT_LOG2);
+    index_num = cache_size / ways_num / block_size;
+    assert( index_num != 0);
+    index_bits = log2( index_num);         // Only power of two is permitted
+    assert( index_bits != NOT_LOG2);
+    tag_bits = addr_size - offset_bits - index_bits;
+    assert( tag_bits >= 0);
 
-	cache_lines = new CacheLine [index_num];
-	for ( uint32 i = 0; i < index_num; i++)
-	{
-		cache_lines[i].tags = new uint32 [ways_num];
-		cache_lines[i].stat = new uint32 [ways_num];
-		for ( uint32 j = 0; j < ways_num; j++)
-			cache_lines[i].stat[j] = ways_num - j - 1;
-	}
-	//NEED DESTRUCTOR!
+    cache_lines = new CacheLine [index_num];
+    for ( uint32 i = 0; i < index_num; i++)
+    {
+        cache_lines[i].tags = new uint32 [ways_num]();
+        cache_lines[i].stat = new uint32 [ways_num]();
+    }
+    access_count = 0;
 }
 
 
+CacheTagArray::~CacheTagArray()
+{
+    for ( uint32 i = 0; i < index_num; i++)
+    {
+        delete cache_lines[i].tags;
+        delete cache_lines[i].stat;
+    }
+    delete cache_lines;
+}
+
+
+/* Access into cache by addr.
+ * Return true if hit, else - miss.
+ */
 bool CacheTagArray::read( uint64 addr)
 {
-	uint32 i;
-	for ( i = 0; i < ways_num; i++)
-		if (cache_lines[getIndex( addr)].tags[i] == getTag( addr))
-			break;
-	if ( i != ways_num)
-	{
-		updateStat( getIndex( addr), i);
-		return true;
-	}
-	else
-	{
-		write( addr);
-		return false;
-	}
+    uint64 index = getIndex( addr);
+    uint64 tag = getTag( addr);
+    // search tag hit
+    for ( uint32 i = 0; i < ways_num; i++)
+        if ( cache_lines[index].tags[i] == tag)
+        {
+            updateStat( index, i);
+            return true;
+        }
+    write( addr);
+    return false;
 }
 
-
+/* Load address into cache in oldest way ( LRU)
+ * update LRU stat
+ */
 void CacheTagArray::write( uint64 addr)
 {
-	uint32 i;
-	for ( i = 0; i < ways_num; i++)
-		if ( cache_lines[getIndex( addr)].stat[i] == ways_num - 1)
-			break;
-	cache_lines[getIndex( addr)].tags[i] = getTag( addr);
-	updateStat( getIndex( addr), i);
+    uint64 index = getIndex( addr);
+    uint64 tag = getTag( addr);
+    uint64 oldest_access_time = -1;         // uint64_max
+    uint32 oldest_way_num = 0;
+    for ( uint32 i = 0; i < ways_num; i++)
+        if ( cache_lines[index].stat[i] < oldest_access_time)
+        {
+            oldest_access_time = cache_lines[index].stat[i];
+            oldest_way_num = i;
+        }
+    cache_lines[index].tags[oldest_way_num] = tag;
+    updateStat( index, oldest_way_num);
 }
 
 
 uint64 CacheTagArray::getTag( uint64 addr)
 {
-	return addr >> (offset_bits + index_bits);
+    return addr >> ( offset_bits + index_bits);
 }
 
 
 uint64 CacheTagArray::getIndex( uint64 addr)
 {
-	return (addr >> offset_bits) & ((1 << index_bits) - 1);
+    return ( addr >> offset_bits) & (( 1 << index_bits) - 1);
 }
 
 
 uint64 CacheTagArray::getOffset( uint64 addr)
 {
-	return addr & ((1 << offset_bits) - 1);
+    return addr & (( 1 << offset_bits) - 1);
 }
 
 
-void CacheTagArray::updateStat(uint32 index, uint32 way)
+/* Update stat for given index, set given way access time
+ */
+void CacheTagArray::updateStat( uint32 index, uint32 way)
 {
-	for ( uint32 i = 0; i < ways_num; i++)
-		if ( cache_lines[index].stat[i] < cache_lines[index].stat[way])
-			cache_lines[index].stat[i]++;
-	cache_lines[index].stat[way] = 0;
+    cache_lines[index].stat[way] = ++access_count;
 }
 
 
--- perf_sim/mem/miss_rate_sim.cpp
+++ perf_sim/mem/miss_rate_sim.cpp
@@ -1,54 +1,69 @@
 /*
- * miss_rate_sim.cpp
- *
+ * miss_rate_sim.cpp - perfomance simulator for 1-level cache
+ * @author Aleksandr Kayda aleksandr.kajda@frtk.ru
+ * Copyright 2014 uArchSim iLab 
  */
 
-
-
+// C++ headers
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <types.h>
-#include <cache_tag_array.h>
+// pure C headers
 #include <errno.h>
 #include <cstdlib>
-#include <unistd.h>
+// our headers
+#include <types.h>
+#include <cache_tag_array.h>
 
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[])
 {
-	if (argc != 2){
-		cout << "need exactly 1 argument: name of file with addresses" << endl;
-		exit(EXIT_FAILURE); 		// is this good style for C++ ?
-	}
+    if ( argc != 2){
+        cout << "need exactly 1 argument: name of file with addresses" << endl;
+        exit( EXIT_FAILURE);         // is this good style for C++ ?
+    }
 
-	ifstream addr_file;
-	addr_file.open(argv[1]);
-	if (!addr_file.is_open())
-	{
-		cerr << "Invalid filename: " << strerror(errno) << endl;
-		exit(EXIT_FAILURE);
-	}
+    ifstream addr_file;
+    addr_file.open( argv[1]);
+    if ( !addr_file.is_open())
+    {
+        cerr << "Invalid filename: " << strerror( errno) << endl;
+        exit( EXIT_FAILURE);
+    }
+    uint32 trace_size = 0;
+    uint32 address;
+    while ( addr_file >> hex >> address)
+        trace_size++;
+    if ( !addr_file.eof())
+    {
+        cerr << "Error reading file: " << strerror( errno) << endl;
+        exit( EXIT_FAILURE);
+    }
+    addr_file.clear();
+    addr_file.seekg( 0);
+    uint32 trace_addr[trace_size];
+    for ( uint32 i = 0; i < trace_size; i++)
+        addr_file >> hex >> trace_addr[i];
+    addr_file.close();
 
-	CacheTagArray cache(4096, 4, 4, 32);
-	uint64 address;
-	uint32 access_count = 0;
-	uint32 misses_count = 0;
-	while ( addr_file >> hex >> address)
-	{
-		if ( !cache.read( address))
-			misses_count++;
-		access_count++;
-	}
-	
-	float miss_rate = (float) misses_count / access_count;
-	cout << miss_rate << endl;
-	cout << access_count << endl;
-	addr_file.close();
+    uint32 way_set[] = { 1, 2, 4, 8, 16, 0};
+    for ( uint32 i = 0; i < 6; i++)
+    {
+        for ( uint32 j = 1024; j <= 1024*1024; j <<= 1)
+        {
+            CacheTagArray cache( j, way_set[i], 4, 32);
+            uint32 misses_count = 0;
+            for ( uint32 k = 0; k < trace_size; k++)
+                if ( !cache.read( trace_addr[k]))
+                    misses_count++;
+            float miss_rate = ( float) misses_count / trace_size;
+            cout << miss_rate << ", ";
+        }
+        cout << endl;
+    }
 
-	return 0;
+    return 0;
 }
-
 
