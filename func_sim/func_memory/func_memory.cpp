87a513e32ee1973c0aefebebfe24cc617023b1f0
alexander.pronin.mipt <alexander.pronin.mipt@gmail.com>
2014-10-16 17:22:54 +0000

    [branch] first version of the first assignment

--- func_sim/func_memory/func_memory.cpp
+++ func_sim/func_memory/func_memory.cpp
@@ -5,41 +5,165 @@
  * Copyright 2012 uArchSim iLab project
  */
 
+  /**
+ * updated by Alexander Pronin <alexander.pronin.mipt@gmail.com>
+ * v. 1.0: created 16.10.2014 21:05
+ *         added implementation of main functions
+ */
+
 // Generic C
+#include <string.h>
 
 // Generic C++
 
 // uArchSim modules
 #include <func_memory.h>
 
+
 FuncMemory::FuncMemory( const char* executable_file_name,
                         uint64 addr_size,
                         uint64 page_bits,
                         uint64 offset_bits)
 {
-    // put your code here
+    assert( addr_size);
+    assert( page_bits);
+    assert( offset_bits);
+    assert( executable_file_name);
+    this->OffsetSize = 1;
+    this->PageSize = 1;
+    this->SetSize = 1;
+    this->OffsetBits = offset_bits;
+    this->PageBits = page_bits;
+    this->SetBits = addr_size - page_bits - offset_bits;
+    this->PCstart_addr = 0;
+    for ( int i = 0; i < offset_bits; i++)
+        this->OffsetSize *= 2;
+    for ( int i = 0; i < page_bits; i++)
+        this->PageSize *= 2;
+    for ( int i = 0; i < ( addr_size - page_bits - offset_bits); i++)
+        this->SetSize *= 2;
+    this->memory = new MemSet* [ SetSize];
+    for ( int i = 0; i < SetSize; i++)
+        this->memory[ i] = NULL;
+    vector<ElfSection> sections_array;
+    ElfSection::getAllElfSections( executable_file_name, sections_array);
+    for ( int i = 0; i < sections_array.size(); i++)
+    {
+        if ( !strcmp( ".text", sections_array[ i].name))
+        {
+            PCstart_addr = sections_array[ i].start_addr;
+        }
+        this->MemWrite( sections_array[ i].content, sections_array[ i].start_addr, sections_array[ i].size);
+    }
 }
+
+
+void FuncMemory::MemWrite( uint8* value, uint64 addr, uint64 size)
+{
+    assert( size);
+    uint64 cur_set_addr = ( ( this->SetSize - 1) & ( addr >> ( this->OffsetBits + this->PageBits)));
+    uint64 cur_page_addr = ( ( this->PageSize - 1) & ( addr >> this->OffsetBits));
+    uint64 cur_offset_addr = ( ( this->OffsetSize - 1) & addr);
+    uint64 pos = 0;
+    while ( size > 0)
+    {
+        if ( this->memory[ cur_set_addr])
+        {
+            if ( this->memory[ cur_set_addr]->page[ cur_page_addr])
+            {
+                this->memory[ cur_set_addr]->page[ cur_page_addr]->offset[ cur_offset_addr].value = value[ pos];
+            }
+            else
+            {
+                this->memory[ cur_set_addr]->page[ cur_page_addr] = new MemPage;
+                this->memory[ cur_set_addr]->page[ cur_page_addr]->offset = new MemOffset[ this->OffsetSize];
+                this->memory[ cur_set_addr]->page[ cur_page_addr]->offset[ cur_offset_addr].value = value[ pos];
+            }
+        }
+        else
+        {
+            this->memory[ cur_set_addr] = new MemSet;
+            this->memory[ cur_set_addr]->page = new MemPage*[ this->PageSize];
+            for ( int i = 0; i < PageSize; i++)
+            {
+                this->memory[ cur_set_addr]->page[ i] = NULL;
+            }
+            this->memory[ cur_set_addr]->page[ cur_page_addr] = new MemPage;
+            this->memory[ cur_set_addr]->page[ cur_page_addr]->offset = new MemOffset[ this->OffsetSize];
+            this->memory[ cur_set_addr]->page[ cur_page_addr]->offset[ cur_offset_addr].value = value[ pos];
+        }
+        addr++;
+        pos++;
+        size--;
+        cur_set_addr = ( ( this->SetSize - 1) & ( addr >> ( this->OffsetBits + this->PageBits)));
+        cur_page_addr = ( ( this->PageSize - 1) & ( addr >> this->OffsetBits));
+        cur_offset_addr = ( ( this->OffsetSize - 1) & addr);
+    }
+}
+
 
 FuncMemory::~FuncMemory()
 {
-    // put your code here
+    for ( int i = 0; i < this->SetSize; i++)
+    {
+        if ( this->memory[ i])
+        {
+            for ( int j = 0; j < this->PageSize; j++)
+            {
+                if ( this->memory[ i]->page[ j])
+                {
+                    delete [] this->memory[ i]->page[ j];
+                }
+            }
+            delete [] this->memory[ i];
+        }
+    }
+    delete [] this->memory;
 }
+
 
 uint64 FuncMemory::startPC() const
 {
-    // put your code here
+    return this->PCstart_addr;
 }
+
 
 uint64 FuncMemory::read( uint64 addr, unsigned short num_of_bytes) const
 {
-    // put your code here
-
-    return 0;
+    assert( num_of_bytes);
+    assert( num_of_bytes <= 8);
+    uint64 cur_set_addr = ( ( this->SetSize - 1) & ( addr >> ( this->OffsetBits + this->PageBits)));
+    uint64 cur_page_addr = ( ( this->PageSize - 1) & ( addr >> this->OffsetBits));
+    uint64 cur_offset_addr = ( ( this->OffsetSize - 1) & addr);
+    uint8 res[ 8];
+    uint64 result = 0;
+    bool done = false;
+    for ( int i = 0; i < num_of_bytes; i++)
+    {
+        assert( this->memory[ cur_set_addr]);
+        assert( this->memory[ cur_set_addr]->page[ cur_page_addr]);
+        res[ i] = this->memory[ cur_set_addr]->page[ cur_page_addr]->offset[ cur_offset_addr].value;
+        addr++;
+        cur_set_addr = ( ( this->SetSize - 1) & ( addr >> ( this->OffsetBits + this->PageBits)));
+        cur_page_addr = ( ( this->PageSize - 1) & ( addr >> this->OffsetBits));
+        cur_offset_addr = ( ( this->OffsetSize - 1) & addr);
+    }
+    for ( int i = num_of_bytes - 1; i >= 0; i--)
+    {
+        result = result * 256 + res[ i];
+    }
+    return result;
 }
+
 
 void FuncMemory::write( uint64 value, uint64 addr, unsigned short num_of_bytes)
 {
-    // put your code here
+    uint8 res[ 8];
+    for ( int i = 0; i < num_of_bytes; i++)
+    {
+        res[ i] = uint8 ( ( value >> ( 8 * i)) & 255);
+    }
+    this->MemWrite( res, addr, num_of_bytes);
 }
 
 string FuncMemory::dump( string indent) const
--- func_sim/func_memory/func_memory.h
+++ func_sim/func_memory/func_memory.h
@@ -5,6 +5,12 @@
  * Copyright 2012 uArchSim iLab project
  */
 
+ /**
+ * updated by Alexander Pronin <alexander.pronin.mipt@gmail.com>
+ * v. 1.0: created 16.10.2014 21:05
+ *         added implementation of main functions
+ */
+
 // protection from multi-include
 #ifndef FUNC_MEMORY__FUNC_MEMORY_H
 #define FUNC_MEMORY__FUNC_MEMORY_H
@@ -19,6 +25,24 @@
 
 using namespace std;
 
+class MemOffset
+{
+public:
+    char value;
+};
+
+class MemPage
+{
+public:
+    MemOffset* offset;
+};
+
+class MemSet
+{
+public:
+    MemPage** page;
+};
+
 class FuncMemory
 {
     // You could not create the object
@@ -27,16 +51,25 @@ class FuncMemory
 
 public:
 
+    MemSet** memory;
+    uint64 SetSize;
+    uint64 PageSize;
+    uint64 OffsetSize;
+    uint64 PCstart_addr;
+    uint64 SetBits;
+    uint64 PageBits;
+    uint64 OffsetBits;
+
     FuncMemory ( const char* executable_file_name,
                  uint64 addr_size = 32,
                  uint64 page_num_size = 10,
                  uint64 offset_size = 12);
-    
+
     virtual ~FuncMemory();
     
     uint64 read( uint64 addr, unsigned short num_of_bytes = 4) const;
     void   write( uint64 value, uint64 addr, unsigned short num_of_bytes = 4);
-    
+    void   MemWrite( uint8* value, uint64 addr, uint64 size);
     uint64 startPC() const;
     
     string dump( string indent = "") const;
