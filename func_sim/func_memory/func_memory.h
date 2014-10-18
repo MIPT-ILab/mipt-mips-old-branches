80e4a68386ef709d67b39c2a9e7310ad0fdc745b
ilya.i.belikov <ilya.i.belikov@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-10-18 19:16:10 +0000

    [branch] the latest update before deadline

--- func_sim/func_memory/func_memory.h
+++ func_sim/func_memory/func_memory.h
@@ -29,10 +29,6 @@ class FuncMemory
     FuncMemory(){}
 
 public:
-    uint64 SetSize;
-    uint64 PageSize;
-    uint64 OffsetSize;
-
     std::map< uint64, const void* > Memory;
 
     FuncMemory ( const char* executable_file_name,
@@ -42,22 +38,29 @@ public:
 
     virtual ~FuncMemory();
 
-    uint64 returnSet( uint64 addr) const;
-    uint64 returnPage( uint64 addr) const;
-    uint64 returnOffset( uint64 addr) const;
-
     uint64 read( uint64 addr, unsigned short num_of_bytes = 4) const;
     void   write( uint64 value, uint64 addr, unsigned short num_of_bytes = 4);
 
     uint64 startPC() const;
 
-    string dump( string indent = "") const;
+    string dump( string indent = "");
 
 private:
-    void write_byte( uint64 Set_addr, uint64 Page_addr, uint64 Offset_addr, uint8 data);
-    uint8 read_byte( uint64 Set_addr, uint64 Page_addr, uint64 Offset_addr) const;
+    void write_s( uint8* data, uint64 addr, unsigned short num_of_bytes);
+    void read_s( uint8* data, uint64 addr, unsigned short num_of_bytes) const;
 
-    uint64 very_first_addr;
+    string Setdump( string indent, const void* pointer);
+    string Pagedump( string indent, const void* pointer);
+
+    uint64 SetSize;
+    uint64 PageSize;
+    uint64 OffsetSize;
+
+    uint64 returnSet( uint64 addr) const;
+    uint64 returnPage( uint64 addr) const;
+    uint64 returnOffset( uint64 addr) const;
+
+    uint64 start_addr;
     uint64 text_start_addr;
 };
 
