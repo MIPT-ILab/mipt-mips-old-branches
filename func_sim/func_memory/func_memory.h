f2ee003f644ae7874097c84675d5f40a7e77f11e
ilya.i.belikov <ilya.i.belikov@7b285467-9990-9c3a-2c35-738ad0b76374>
2014-10-18 08:07:10 +0000

    [branch] dump method is not implemented

--- func_sim/func_memory/func_memory.h
+++ func_sim/func_memory/func_memory.h
@@ -12,14 +12,10 @@
 // Generic C++
 #include <string>
 #include <cassert>
-#include <iostream>
-#include <sstream>
 
 // uArchSim modules
 #include <types.h>
 #include <elf_parser.h>
-
-#include <vector>
 
 using namespace std;
 
@@ -28,12 +24,6 @@ class FuncMemory
     // You could not create the object
     // using this default constructor
     FuncMemory(){}
-    uint64 addr_size_tmp;
-    uint64 page_bits_tmp;
-    uint64 offset_bits_tmp;
-    vector<vector<vector<uint8> > > memory;
-    uint64 start_addres;
-        
 
 public:
 
@@ -52,6 +42,4 @@ public:
     string dump( string indent = "") const;
 };
 
-uint64 maskfunc(uint64 addr_size_len, uint64 size_right, uint64 size);
-uint8 invert(uint8 var);
 #endif // #ifndef FUNC_MEMORY__FUNC_MEMORY_H
