163505484958ba7da411856c252f9569ac391fa8
ermilov <ermilov@phystech.edu>
2014-12-06 20:29:04 +0000

    [branch] last version: disasm.cpp is improved

--- func_sim/func_instr/disasm.cpp
+++ func_sim/func_instr/disasm.cpp
@@ -1,9 +1,17 @@
+/** 
+ * disasm.cpp - this program reads bytes of the section from the file and gives them for parsing
+ * @author Dmitry Ermilov <ermilov@phystech.edu>
+ * Copyright 2012 uArchSim iLab project, 2012 uArchSim iLab project
+ */
+// Genereic C
 #include <cassert>
 #include <cstdlib>
 #include <cstring> 
 
+// Generic C++
 #include <iostream>
 
+// uArchSim modules
 #include <func_memory.h>
 #include <func_instr.h>
 
@@ -16,21 +24,27 @@ int main( int argc, char* argv[])
 	if ( argc - 1 == num_of_args)
     {
         const char* file_name = argv[1];
-        const char* section = argv[2];        
+        const char* section = argv[2];
+
+        FuncMemory func_mem( file_name, 32, 10, 12);       
     
         vector<ElfSection> sections_array;
-        ElfSection::getAllElfSections( argv[1], sections_array); 
+        ElfSection::getAllElfSections( file_name, sections_array); 
 
         int i;
-        for ( i = 0; strcmp(sections_array[i].name, section); i++);
-
-        uint8* ptr = sections_array[i].content;
-        uint64 address = sections_array[i].start_addr;
-        for ( int j = 0; j < sections_array[i].size; j++, ptr++)
+        for ( i = 0; strcmp(sections_array[i].name, section); i++, cout << i);   //we find the number of section
+//        cout << sections_array[i].start_addr << endl;
+//        uint8* ptr = sections_array[i].content;
+        uint64 address = sections_array[i].start_addr;                           //we remember the start address
+        for ( int j = 0; j < sections_array[i].size; j++)
         {
-//            FuncInstr instr( func_mem.read( address, 1));
-            FuncInstr instr( *ptr);
-            cout << instr;
+            FuncInstr instr( func_mem.read( address, 1));                        //we read 1 byte and parse it
+            cout << j << address <<  endl;
+//            cout << *ptr << endl;
+//            FuncInstr instr( *ptr);
+//            ptr++;
+            cout << instr;                                                       //we putout disasmbled byte
+            address++;
         }
     } 
     else if ( argc - 1 > num_of_args)
