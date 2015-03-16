e91aba19fd7a6e090dcf183e34f8edd9309aac7a
anna.saenko <anna.saenko@phystech.edu>
2015-03-16 19:00:29 +0000

    [branch] 'func_sim.h' added

---
+++ func_sim/func_sim.h
@@ -0,0 +1,65 @@
+
+#ifndef FUNC_SIM_H
+#define FUNC_SIM_H
+
+
+#include <types.h>
+#include <func_memory.h>
+#include <func_instr.h>
+
+enum RegNum {
+    $zero,
+    $at,
+    $v0, $v1,
+    $a0, $a1, $a2, $a3,
+    $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7,
+    $s0, $s1, $s2, $s3, $s4, $s5, $s6, $s7,
+    $t8, $t9,
+    $k0, $k1,
+    $gp,
+    $sp,
+    $fp,
+    $ra,
+    MAX_REG = 32
+};
+
+class FuncInstr;
+
+class RF {
+        uint32 array[MAX_REG];
+    public:
+        RF();
+
+        uint32 read( RegNum index);
+        void write( RegNum index, uint32 data);
+        void reset( RegNum index)
+        void dump( char * file);
+        void resetAll() { for(int i = 0; i < MAX_REG; i++)    array[i] = 0;   }
+};
+
+class MIPS {
+
+    friend class FuncInstr;
+        RF* rf;
+        uint32 PC;
+        FuncMemory* mem;
+        void load( FuncInstr& instr);
+        void store( const FuncInstr& instr);
+        void read_src( FuncInstr& instr);
+        void ld_st( FuncInstr& instr);
+        void wb( FuncInstr& instr);
+
+    public:
+        MIPS();
+        ~MIPS();
+
+        void run( const string&, uint32 instr_to_run);
+        uint32 fetch() { return (uint32)mem->read(PC); };
+        void updatePC( const FuncInstr& instr);
+        uint32 read(RegNum index);
+
+
+
+};
+
+ #endif // FUNC_SIM_H
