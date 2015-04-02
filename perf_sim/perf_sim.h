f1630a96c13833284e73420bd56daf03271dafd8
alexander.pronin.mipt <alexander.pronin.mipt@gmail.com>
2015-04-02 23:37:13 +0000

    [branch] continue working with the task, changes in changelog

--- perf_sim/perf_sim.h
+++ perf_sim/perf_sim.h
@@ -14,62 +14,102 @@
 
 #include <ports.h>
 #include <func_instr.h>
-#include <func_sim.h>
+#include <func_mem.h>
 #include <types.h>
+#include <rf.h>
 
 class PerfMIPS {
+        RF* rf;                                        // registers
+        uint32 PC;                                     // current PC
+        FuncMemory* mem;                               // memory
 
-    void run(/* */);                               // start emulation
-    PerfMIPS();                                    // prepare ports to work
-    ~PerfMIPS();
+        int executed_instrs;                           // number of executed instructions
 
-    int executed_instrs;                           // number of executed instructions
-
-    // fetch
-    WritePort<uint32>* wp_fetch_2_decode;          // write fetch to decode
+        // fetch
+        WritePort<uint32>* wp_fetch_2_decode;          // write fetch to decode
     
-    ReadPort<bool>* rp_decode_2_fetch_stall;       // read decode to fetch stall
+        ReadPort<bool>* rp_decode_2_fetch_stall;       // read decode to fetch stall
     
-    FuncInstr* fetchInstr;                         // current instruction in fetch
-    void clock_fetch( int cycle);                  // execute fetch
+        FuncInstr* fetchInstr;                         // current instruction in fetch
+        void clock_fetch( int cycle);                  // execute fetch block
 
-    // decode
-    ReadPort<uint32>*     rp_fetch_2_decode;       // read fetch to decode
-    WritePort<FuncInstr>* wp_decode_2_execute;     // write decode to execute
+        uint32 fetch() const { return mem->read(PC); } // execute fetch
 
-    ReadPort<bool>*  rp_execute_2_decode_stall;    // read execute to decode stall
-    WritePort<bool>* wp_decode_2_fetch_stall;      // write decode to fetch stall
+        // decode
+        ReadPort<uint32>*     rp_fetch_2_decode;       // read fetch to decode
+        WritePort<FuncInstr>* wp_decode_2_execute;     // write decode to execute
 
-    FuncInstr* decodeInstr;                        // current instruction in decode
-    void clock_decode( int cycle);                 // execute decode
+        ReadPort<bool>*  rp_execute_2_decode_stall;    // read execute to decode stall
+        WritePort<bool>* wp_decode_2_fetch_stall;      // write decode to fetch stall
 
-    // execute
-    ReadPort<FuncInstr>*  rp_decode_2_execute;     // read decode to execute
-    WritePort<FuncInstr>* wp_execute_2_memory;     // write execute to memory
+        FuncInstr* decodeInstr;                        // current instruction in decode
+        void clock_decode( int cycle);                 // execute decode block
 
-    ReadPort<bool>*  rp_memory_2_execute_stall;    // read memory to execute stall
-    WritePort<bool>* wp_execute_2_decode_stall;    // write execute to decode stall
+        void read_src( FuncInstr& instr) const {       // read sources
+            size_t reg_num = instr.get_src1_num();
+            instr.set_v_src1( rf->read( ( Reg_Num) reg_num));
 
-    FuncInstr* executeInstr;                       // current instruction in execute
-    void clock_execute( int cycle);                // execute execute
+            reg_num = instr.get_src2_num();
+            instr.set_v_src2( rf->read( ( Reg_Num) reg_num));
+	    }
 
-    // memory
-    ReadPort<FuncInstr>*  rp_execute_2_memory;     // read execute to memory
-    WritePort<FuncInstr>* wp_memory_2_writeback;   // write memory to writeback
+        // execute
+        ReadPort<FuncInstr>*  rp_decode_2_execute;     // read decode to execute
+        WritePort<FuncInstr>* wp_execute_2_memory;     // write execute to memory
 
-    ReadPort<bool>* rp_writeback_2_memory_stall;   // read writeback to memory stall
-    WritePort<bool>* wp_memory_2_execute_stall;     // write memory to execute stall
+        ReadPort<bool>*  rp_memory_2_execute_stall;    // read memory to execute stall
+        WritePort<bool>* wp_execute_2_decode_stall;    // write execute to decode stall
 
-    FuncInstr* memoryInstr;                        // current instruction in memory
-    void clock_memory( int cycle);                 // execute memory
+        FuncInstr* executeInstr;                       // current instruction in execute
+        void clock_execute( int cycle);                // execute execute block
 
-    // writeback
-    ReadPort<FuncInstr>* rp_memory_2_writeback;    // read memory to writeback
+        // memory
+        ReadPort<FuncInstr>*  rp_execute_2_memory;     // read execute to memory
+        WritePort<FuncInstr>* wp_memory_2_writeback;   // write memory to writeback
+
+        ReadPort<bool>* rp_writeback_2_memory_stall;   // read writeback to memory stall
+        WritePort<bool>* wp_memory_2_execute_stall;    // write memory to execute stall
+
+        FuncInstr* memoryInstr;                        // current instruction in memory
+        void clock_memory( int cycle);                 // execute memory block
+
+        void load( FuncInstr& instr) const {           // load data
+            instr.set_v_dst( mem->read( instr.get_mem_addr(), instr.get_mem_size()));
+        }
+
+        void store( const FuncInstr& instr) {          // store data
+            mem->write( instr.get_v_src2(), instr.get_mem_addr(), instr.get_mem_size());
+        }
+
+    	void load_store( FuncInstr& instr) {           // work with memory
+            if ( instr.is_load())
+                load( instr);
+            else if ( instr.is_store())
+                store( instr);
+        }
+
+        // writeback
+        ReadPort<FuncInstr>* rp_memory_2_writeback;    // read memory to writeback
     
-    WritePort<bool>* wp_writeback_2_memory_stall;  // write writeback to memory stall
+        WritePort<bool>* wp_writeback_2_memory_stall;  // write writeback to memory stall
 
-    FuncInstr* writebackInstr;                     // current instruction in writeback
-    void clock_writeback( int cycle);              // execute writeback
+        FuncInstr* writebackInstr;                     // current instruction in writeback
+        void clock_writeback( int cycle);              // execute writeback block
+
+        void wb( const FuncInstr& instr) {             // do writeback
+            rf->write_dst( instr);
+        }
+
+    public:
+        // start
+        void run( const std::string& tr, uint32 instrs_to_run, bool silent);
+        
+        // prepare ports to work
+        PerfMIPS();
+
+        // free allocated memory
+        ~PerfMIPS();
+
 };
 
 #endif // PERF_SIM_H
---
+++ perf_sim/rf.h
@@ -0,0 +1,45 @@
+/*
+ * rf.h - mips register file
+ * @author Pavel Kryukov pavel.kryukov@phystech.edu
+ * modified by Alexander Pronin <alexander.pronin.mipt@gmail.com>
+ * Copyright 2015 MIPT-MIPS 
+ */
+
+#ifndef RF_H
+#define RF_H
+
+#include <func_instr.h>
+
+class RF
+{
+        struct Reg {
+            uint32 value;
+            bool   is_valid;
+            Reg() : value( 0ull), is_valid( true) { }    
+        } array[ REG_MAX_NUM];
+    public:
+        uint32 read( Reg_Num num) { return array[ ( size_t) num].value; }
+
+        bool check( Reg_Num num) const { return array[ ( size_t) num].is_valid; }
+
+        void invalidate( Reg_Num num) { array[ ( size_t) num].is_valid = false; }   // zero register checks are in perf_mips
+
+        void write ( Reg_Num num, uint32 val) {                                     // zero register checks are in perf_mips
+            assert( array[(size_t)num].is_valid == false);
+            array[ ( size_t) num].value    = val;
+            array[ ( size_t) num].is_valid = true;
+        }
+
+        void reset( RegNum reg) {
+            array[ reg].is_valid = true;
+            array[ reg].value = 0;
+        }
+ 
+        RF() {
+            for ( size_t i = 0; i < REG_NUM_MAX; ++i)
+                reset( ( RegNum) i);
+        }
+};
+          
+#endif
+ 
